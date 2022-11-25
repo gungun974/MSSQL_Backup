d14889219d62d595f87112b695fc2f71aebb9f96
Gungun974 <xfelix974@gmail.Com>
2022-11-25 23:15:26 +0400

    Add weekly backup

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import cron from 'node-cron';
 import { MainLogger } from './Logger';
 import { keepLastFile, makeNewBackup } from './core';
 
-const dailyCron = process.env.DAILY_CRON || '4 4 * * *';
+const dailyCron = process.env.DAILY_CRON || '4 2 * * *';
+const weeklyCron = process.env.WEEKLY_CRON || '4 4 * * 0';
 const monthlyCron = process.env.MONTHLY_CRON || '6 4 1 * *';
 const yearlyCron = process.env.YEARLY_CRON || '8 4 1 1 *';
 
@@ -10,7 +11,13 @@ const yearlyCron = process.env.YEARLY_CRON || '8 4 1 1 *';
 cron.schedule(dailyCron, async () => {
   MainLogger.info('Execute', dailyCron);
   await makeNewBackup('day');
-  await keepLastFile('day', 30);
+  await keepLastFile('day', 7);
+});
+
+cron.schedule(dailyCron, async () => {
+  MainLogger.info('Execute', weeklyCron);
+  await makeNewBackup('weekly');
+  await keepLastFile('weekly', 4);
 });
 
 cron.schedule(monthlyCron, async () => {
